e16ecc8b23d85abcf28a014ebe32f40dbcc3afcc
Steven McKeown <steven@pragmaflow.com>
2021-11-10 10:45:17 -0500

    fixed ios script

--- scripts/install_ios.sh
+++ scripts/install_ios.sh
@@ -2,6 +2,13 @@
 set -e
 set -x
 
-plutil -insert NSFaceIDUsageDescription -string 'Enabling Face ID allows you quick and secure access to your account.' info.plist
+name=$PROJECT_NAME
+
+if grep -q "<key>NSFaceIDUsageDescription" ios/$name/Info.plist; then
+  echo "Biometrics already supported, nothing to do here."
+else
+  plutil -insert NSFaceIDUsageDescription -string 'Enabling Face ID allows you quick and secure access to your account.' ios/$name/Info.plist
+fi
+
 
 echo "configured iOS settings"
